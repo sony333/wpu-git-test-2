198c01144ab1755e3896ab5b3b05a7a5fa5d1cf3
sony333 <triprasetyosony7@gmail.com>
2024-10-07 18:36:20 +0700

    Menambahkan list

--- Index.php
+++ Index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit dolore, eveniet dolorum repellat deserunt odio natus labore ut alias id aliquam eligendi dolorem quisquam nam voluptatum, tempora accusamus obcaecati? Beatae.</p>
+    <ul>
+        <li>Daftar 1</li>
+        <li>Daftar 2</li>
+    </ul>
 </body>
 
 </html>
